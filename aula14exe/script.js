cfe504d9d3a7001b62dfad9fe837d161f9221610
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-05-05 08:26:41 -0300

    tabuada

--- aula14exe/script.js
+++ aula14exe/script.js
@@ -4,46 +4,36 @@ function contar() {
     var pass = document.getElementById('txtp')
     var res = document.getElementById('res')
 
-    if (ini.value.length == 0, fim.value.length == 0, pass.value.length == 0) {
+    if (ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0) {
 
-        alert('[ERRO!!] - Faltam dados')
-
+        alert('[ERRO] - Dados Incompletos')
     } else {
-        res.innerHTML = 'Contando ... <br>'
+        res.innerHTML = 'Contando ... '
 
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(pass.value)
+    var i = Number(ini.value)
+    var f = Number(fim.value)
+    var p = Number(pass.value)
 
-        if (p <= 0) {
+    if ( p <= 0) {
+        p = 1
+       
+        alert('Passos Invalidos. Será considerando o passo 1 ')
+    }
 
-            alert('Passo Invalido, Considerando passo 1')
-            
-            p = 1
+    if (i < f) {
+        for (var c = i ; c <= f ; c = c + p) {
 
+            res.innerHTML += ` ${c} \u{1f9b6}`
         }
-        if (i < f) {
-
-            for (var c = i; c <= f; c = c + p) {
-
-                res.innerHTML += `${c} \u{1f9b6}`
-    
-            }
     } else {
- 
-            for (var c = i ; c >= f ; c = c - p ){
-
-                res.innerHTML += `${c} \u{1f9b6}`
-            }
-
-
-    } 
-
+        for (var c = i ; c >= f ; c = c - p) {
+            res.innerHTML += `${c} \u{1f9b6}`
         }
+    }
 
-        res.innerHTML += `\u{1f441}`
-
+    res.innerHTML += `${c} \u{1f3c1}`
     }
 
 
+}
 
