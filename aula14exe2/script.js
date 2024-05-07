776fa109e036b23ced496afb13477cab94fed1b0
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-05-06 21:03:42 -0300

    TABUADA

--- aula14exe2/script.js
+++ aula14exe2/script.js
@@ -5,22 +5,16 @@ function tabuada() {
     if (num.value.length == 0) {
 
         alert('Favor digitar um numero para tabuada')
-        
-    }else {
 
-    var n = Number(num.value)
-    c = 1
-    tab.innerHTML = ``
-
-    while (c <= 10 ) {
-
-        var item = document.createElement(`option`)
-        item.text = `${n} x ${c} = ${n*c}`
-        tab.appendChild(item)
-        c++
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ' '
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            tab.appendChild(item)
+            c = c + 1
+        }
     }
-
-}
-
-
 }
