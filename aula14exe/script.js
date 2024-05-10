02ae83b6c6a5654080ef8357d250c4105e3a22d5
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-05-09 21:20:07 -0300

    contar

--- aula14exe/script.js
+++ aula14exe/script.js
@@ -1,39 +1,44 @@
-function contar() {
-
+function conte() {
     var ini = document.querySelector('#txti')
     var fim = document.querySelector('#txtf')
     var pass = document.querySelector('#txtp')
     var res = document.querySelector('#res')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0) {
+    if ( ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0) {
 
-        alert('[ERRO] - Dados incompletos ')
-    
-    } else {
+        alert('ERRO - Favor digitar os dados corretos')
+        
+    }else {
 
-        res.innerHTML = " Contando ... <br>"
+        res.innerHTML = "Contando. . . "
 
         var i = Number(ini.value)
         var f = Number(fim.value)
         var p = Number(pass.value)
 
-        if (p <= 0) {
-            alert('Passo incompleto , será considerado o passo 1')
-            p=1
+        if (p <= 0) { 
+
+            alert('Passos invalidos , iniciar com passo 1')
+
+            p = 1
         }
 
         if (i <= f) {
-            for (var c = i; c <= f; c = c + p ) {
 
-                res.innerHTML += ` ${c} \u{1f9b6} `
+            for (var c = i ; c <= f ; c = c + p) {
+
+                res.innerHTML += `${c} \u{1f7e2}`
             }
+
         }else {
-            for (var c = i ; c >= f ; c = c - p) {
+            for (var c = i ; c >=f ; c = c - p) {
 
-                res.innerHTML += ` ${c} \u{1f9b6} `
-
+                res.innerHTML += `${c} \u{1f534}`
             }
         }
-        res.innerHTML += `\u{1f3c1} `
+
+        res.innerHTML += `\u{1f534}`
+
     }
+
 }
--- aula14exe2/script.js
+++ aula14exe2/script.js
@@ -3,21 +3,22 @@ function tabuada() {
     let num = document.querySelector('#txtn')
     let tab = document.querySelector('#seltab')
 
-    if (num.value == 0) {
+    if (num.value.length == 0) {
 
-        alert('Por favor digite um numero')
+        alert('Favor digitar um numero para tabuada')
+
     }
     else {
 
-       var c = 1
-       tab.innerHTML = ''
-        while (c <= 10) {
+        var c = 1
+        tab.innerHTML = " "
+        while (c <= 10){
+
             var n = Number(num.value)
             var item = document.createElement('option')
-             item.text = `${n} x ${c} = ${n*c}`
-             tab.appendChild(item)
-             c = c + 1 
+            item.text = `${n} x ${c} = ${c * n}`
+            tab.appendChild(item)
+            c = c + 1 
         }
     }
-
 }
