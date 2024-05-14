855d05d0726d8cd52d62d13352ce4574c2f20ab1
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-05-13 22:49:44 -0300

    contandor

--- aula14exe/script.js
+++ aula14exe/script.js
@@ -1,4 +1,37 @@
 function contador () {
 
+    let ini = document.querySelector('#txti')
+    let fim = document.querySelector('#txtf')
+    let pass = document.querySelector('#txtp')
+    let res = document.querySelector('#res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 ||pass.value.length == 0) {
+
+            alert('Erro - Favor digitar os dados')    
+    }else {
+        
+        res.innerHTML = 'Contando ... '
+    }
+
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(pass.value)
+
+    if (p <)
+
+    if (i < f) {
+
+    for (let c = i; c <= f ; c = c + p) {
+
+        res.innerHTML += ` ${c} \u{1f49a} `
     
+    }
+    }else {
+        for (let c = i ; c >= f ; c = c - p) {
+            res.innerHTML += ` ${c} \u{1f9e1}`
+        }
+
+       
+    }
+    res.innerHTML += ` F I M `
 }
