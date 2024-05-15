93258e521e5d240b39d8369e3f3bd3058c48a0e2
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-05-15 19:03:34 -0300

    vetor

--- aula15/node_b4f960d8b1945.tmp.js
+++ aula15/node_b4f960d8b1945.tmp.js
@@ -4,9 +4,9 @@ console.log(`A quantidade de nomes no Vetor são ${dados.length}`)
 
 let nome = dados
 
-for (nome = 0 ; nome < dados.length ; nome = nome +1) {
+for (nome = 0 ; nome < dados.length ; nome++) {
 
     console.log(`Na posição ${nome}, está a ${dados[nome]} `)
-    
+    nome = 1
 
 }
