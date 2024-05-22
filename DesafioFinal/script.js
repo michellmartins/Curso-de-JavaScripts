079f9b66a3ef7a46aa8771af9aab716918f99529
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-05-22 19:04:03 -0300

    desafio final

--- DesafioFinal/script.js
+++ DesafioFinal/script.js
@@ -3,91 +3,65 @@ let tab = document.querySelector('#instab')
 let res = document.querySelector('#res')
 let valores = []
 
-
 function insNumero(n) {
 
     if (Number(n) >= 1 && Number(n) <= 100) {
 
         return true
-    } else {
+    }else {
         return false
     }
-
 }
 
-function insLista(n, l) {
-
+function insLista (n,l) {
+    
     if (l.indexOf(Number(n)) != -1) {
+
         return true
+
     } else {
 
         return false
     }
-
 }
-
-
-
 
 function analisar() {
 
-    if (insNumero(num.value) && !insLista(num.value, valores)) {
-
+    if (insNumero(num.value) && !insLista(num.value,valores)) {
 
         valores.push(Number(num.value))
+
+
         let item = document.createElement('option')
-        item.text = ` O valor inserido foi ${num.value}`
+        item.text = `O ${num.value} foi adcionado`
         tab.appendChild(item)
         res.innerHTML = ' '
-    } else {
-        alert('Valor incorreto ou já inserido')
+
+  }  else {
+
+        alert('Numero invalido ou já inserido')
     }
 
-    num.value = ' '
+    num.value = " "
     num.focus()
-
 }
 
+function finalizar () {
 
-function finalizar() {
+        if (valores.length == 0) {
 
-    if (valores.length == 0) {
+            alert('Favor inserir um valor acima')
 
-        alert('Favor digitar um numero acima')
-
-    } else {
-
-        let tot = valores.length
-        let maior = valores[0]
-        let menor = valores[0]
-        let soma = 0
-        let media = 0
-
-
-
-
-
-        for (let pos in valores) {
-
-            soma = soma + valores[pos]
-            media = soma/tot 
-
-            if (valores[pos] > maior) 
-                maior = valores[pos]
-            
-            if (valores[pos] < menor) 
-                menor = valores[pos]
-            }
-      
-    
-
-
-
-    res.innerHTML = ' '
-    res.innerHTML += `<p>Ao todo , temos ${tot} números cadastrados<p>`
-    res.innerHTML += `<p>O maior valor informado foi ${maior}<p>`
-    res.innerHTML += `<p>O menor valor informado foi ${menor}<p>`
-    res.innerHTML += `<p>A soma de todos os valor é  ${soma}<p>`
-    res.innerHTML += `<p>A media dos valores é  ${media}<p>`
         }
+        else {
+
+            let tot = valores.length
+
+            res.innerHTML = " "
+
+            res.innerHTML += (`O total de elementos ${tot}`)
+            
+        }
+
+        
 }
