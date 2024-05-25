ff6ef3543817bbb3a4b6a43abddd2fc4777e40b5
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-05-24 21:22:57 -0300

    desafio final

--- DesafioFinal/script.js
+++ DesafioFinal/script.js
@@ -1,4 +1,4 @@
-let num = document.querySelector('#txtn')
+let num = document.querySelector("#txtn")
 let tab = document.querySelector('#instab')
 let res = document.querySelector('#res')
 let valores = []
@@ -8,22 +8,27 @@ function insNumero(n) {
     if (Number(n) >= 1 && Number(n) <= 100) {
 
         return true
+
     }else {
+
         return false
-    }
+} 
 }
 
-function insLista (n,l) {
-    
-    if (l.indexOf(Number(n)) != -1) {
+function insLista(n,l) {
 
-        return true
+    if(l.indexOf(Number(n)) != -1) {
+
+        return true 
 
     } else {
 
         return false
+
     }
+
 }
+
 
 function analisar() {
 
@@ -31,37 +36,53 @@ function analisar() {
 
         valores.push(Number(num.value))
 
-
         let item = document.createElement('option')
-        item.text = `O ${num.value} foi adcionado`
-        tab.appendChild(item)
+        item.text = `O Valor inserido foi ${num.value}`
+        tab.appendChild(item) 
         res.innerHTML = ' '
+       
+        
+       
 
-  }  else {
-
-        alert('Numero invalido ou já inserido')
+    } else {
+    
+        alert( 'Valor Invalido ou já foi inserido')
+        
+        
     }
 
     num.value = " "
     num.focus()
+    
+    
 }
 
-function finalizar () {
 
-        if (valores.length == 0) {
 
-            alert('Favor inserir um valor acima')
+function finalizar() {
 
-        }
-        else {
+    if ( valores.length == 0) {
 
-            let tot = valores.length
+        alert('Favor digitar uma valor acima')
 
-            res.innerHTML = " "
+    }else {
+        
 
-            res.innerHTML += (`O total de elementos ${tot}`)
-            
-        }
+        tot = valores.length
+        let maior = 0
+        let menor = 0
+        let soma = 0
 
         
+        
+        res.innerHTML = " "
+        res.innerHTML += `O valor total de elementos é ${tot}`
+       
+    }
+
+    
+        
+    
 }
+
+
