910fb1ba573c075d797324141fc53a55b83b5a15
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-06-10 22:09:16 -0300

    desafio

---
+++ Objeto/objeto.js
@@ -0,0 +1,12 @@
+const carro = {
+    marca: 'Ford',
+    modelo:'Mustang',
+    ano: 2024,
+    cor: 'Preta',
+    acelerar: function() {
+
+        console.log('Vrummm, o Mustang está acelerando ')
+    }
+}
+carro.acelerar()
+
