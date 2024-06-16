9c9560f6e21011c0eea8037096be84e44c81f15d
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-06-16 20:32:44 -0300

    desafio

--- DesafioFinal/script.js
+++ DesafioFinal/script.js
@@ -5,13 +5,15 @@ let valores = []
 
 function insNumero(n) {
 
-    if (Number(n) >=1 && Number(n) <= 100) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
 
-        return true
+        return true 
     }else {
 
         return false
+
     }
+
 }
 
 function insLista(n,l) {
@@ -19,72 +21,73 @@ function insLista(n,l) {
     if (l.indexOf(Number(n)) != -1) {
 
         return true 
+
     }else {
 
         return false
     }
+    
 }
-
-
 
 function analisar () {
 
     if (insNumero(num.value) && !insLista(num.value,valores)) {
 
-        valores.push(Number(num.value)) 
-
+        valores.push(Number(num.value))
         let item = document.createElement('option')
-        item.text = `O valor é ${num.value}`
+        item.text = `O valor inserido é ${num.value}`
         lista.appendChild(item)
         res.innerHTML = " "
 
     }else {
 
-        alert ('ERRO - Valor invalido ou Já inserido')
-    }
+        alert ( 'Valor ja inserido ou Valor Invalido') 
 
-    num.value = " "
+    }
+    num.value = ' '
     num.focus()
 }
 
 function finalizar () {
 
-    if (valores.length == 0 ) {
-
-        alert (' Favor digitar entre 1 a 100 ')
+    let total = valores.length
+    let maior = valores[0]
+    let menor = valores[0]
+    let soma = 0
+    let media = 0
     
-    }else{
 
-        let total = valores.length 
-        let maior = valores[0]
-        let menor = valores[0]
-        let media = 0
-        let soma = 0
+    if ( total == 0 ) {
+
+        alert('Favor digitar o valor entre 1 a 100') 
+
+    }else {
 
         for (let pos in valores) {
 
-            if (valores[pos] > maior  ) {
+            if (valores[pos] > maior) {
 
                 maior = valores[pos]
             }
-            if (valores[pos] < menor ) {
+            if ( valores[pos] < menor ) {
 
-                menor = valores[pos]
+                menor = valores[pos] 
             }
 
             soma = soma + valores[pos]
             media = soma / total
-
-            
         }
-        res.innerHTML = ' '
-        res.innerHTML += `Os valores digitados foram ${total}`
+
+        res.innerHTML = " "
+        res.innerHTML += `Os valores digitados no total de ${total}`
         res.innerHTML += `<p>O maior valor é ${maior}`
-        res.innerHTML += `<p> O menor valor é ${menor}`
-        res.innerHTML += `<p> A soma dos valores é ${soma}`
-        res.innerHTML += `<p> Os valores digitados em sequencia foram ${valores}`
-        res.innerHTML += `<p> Os valores digitados na ordem crescente ${valores.sort((a,b) => a-b ) }`
-        
+        res.innerHTML += `<p>O menor valor é ${menor}`
+        res.innerHTML += `<p>A soma dos valores é ${soma}`
+        res.innerHTML += `<p>A média dos valores é ${media} `
+        res.innerHTML += `<p>A sequencia computada foi ${valores}`
+        res.innerHTML += `<p>A sequencia na ordem ${valores.sort()}`
+        res.innerHTML += `<p>A sequencia na ordem crescente${valores.sort((a,b) => a-b )}`
+
 
 
 
@@ -92,5 +95,6 @@ function finalizar () {
 
 
     }
-}
 
+
+}
