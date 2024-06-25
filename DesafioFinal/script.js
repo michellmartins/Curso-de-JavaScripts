e04f042c7b1d91f741fad084becb62e25d788405
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-06-25 20:59:45 -0300

    DESAFIO

--- DesafioFinal/script.js
+++ DesafioFinal/script.js
@@ -1,10 +1,20 @@
-let estadosNorte = [ 'Acre', 'Amapa', 'Amazonas', 'Paraná','Rodonia','Roraima','Tocantins' ] 
+let megaSena = [ 25, 14,34,3,15,2 ]
 
+let jogados = [ 15, 10, 25, 34 , 2 , 3]
 
-console.log(`${ estadosNorte}`)
+let acertado = []
 
-estadosNorte[3] = 'Pará'
+console.log(`****** MEGA-SENA ******`)
+for (indexMega = 0 ; indexMega < megaSena.length ; indexMega++) {   
+    for (indexJogados = 0 ; indexJogados < jogados.length ; indexJogados++) 
 
-console.log(`${estadosNorte}`)
+        if (megaSena[indexMega] === jogados[indexJogados]) {
 
-
+            acertado.push(megaSena[indexMega])
+        }
+    }
+console.log(`Os Números da Mega-Sena são compostos por ${megaSena.length} números`)
+console.log(`Os Números da Mega-Sena sorteados foram ${megaSena}`)
+console.log(`Os Números jogados foram ${jogados}`)
+console.log(`Você acertou ${acertado.length}`)
+console.log(`Foram eles ${acertado}`)
