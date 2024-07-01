4bc69c8b443b8b09b75098d31c56c3de11fa1a14
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-07-01 20:42:37 -0300

    desafio

--- DesafioFinal/script.js
+++ DesafioFinal/script.js
@@ -1,20 +1,102 @@
-let megaSena = [ 25, 14,34,3,15,2 ]
+let num = document.querySelector('#txtn')
+let lista = document.querySelector('#instab')
+let res = document.querySelector('#res')
+let valores = []
 
-let jogados = [ 15, 10, 25, 34 , 2 , 3]
+function insNumero(n) {
 
-let acertado = []
+  if (Number(n) >= 1 && Number(n) <= 100) {
 
-console.log(`****** MEGA-SENA ******`)
-for (indexMega = 0 ; indexMega < megaSena.length ; indexMega++) {   
-    for (indexJogados = 0 ; indexJogados < jogados.length ; indexJogados++) 
+    return true
 
-        if (megaSena[indexMega] === jogados[indexJogados]) {
+  } else {
 
-            acertado.push(megaSena[indexMega])
-        }
+    return false
+  }
+}
+
+function insLista(n, l) {
+
+  if (l.indexOf(Number(n)) != -1) {
+
+    return true
+
+  } else {
+
+    return false
+  }
+}
+
+
+function analisar() {
+
+  if (insNumero(num.value) && !insLista(num.value, valores)) {
+
+    valores.push(Number(num.value))
+    item = document.createElement('option')
+    item.text = `O valor é ${num.value}`
+    lista.append(item)
+
+
+  } else {
+
+    alert('ERRO - Valor indevido ou Valor já inserido')
+  }
+
+  num.value = " "
+  num.focus()
+}
+
+function finalizar() {
+
+  let total = valores.length
+  let maior = valores[0]
+  let menor = valores[0]
+  let soma = 0
+  let media = 0
+
+
+
+  if (total == 0) {
+
+    alert('Favor digitar enter 1 a 100')
+
+  } else {
+
+  
+
+    for (let pos in valores) {
+
+      if (valores[pos] > maior) {
+
+        maior = valores[pos]
+
+      }
+
+      if (valores[pos] < menor) {
+
+        menor = valores[pos]
+      }
+
+      soma = soma + valores[pos]
+      media = soma / total
     }
-console.log(`Os Números da Mega-Sena são compostos por ${megaSena.length} números`)
-console.log(`Os Números da Mega-Sena sorteados foram ${megaSena}`)
-console.log(`Os Números jogados foram ${jogados}`)
-console.log(`Você acertou ${acertado.length}`)
-console.log(`Foram eles ${acertado}`)
+
+    res.innerHTML = `Os Números no total foram ${total}`
+    res.innerHTML += `<p> O maior numero é ${maior}`
+    res.innerHTML += `<p> O menor numero é ${menor}`
+    res.innerHTML += `<p> O Soma dos valores é ${soma}`
+    res.innerHTML += `<p> A média dos valores é ${media}`
+    res.innerHTML += `<p> Os valores na sequencia é ${valores}`
+    res.innerHTML += `<p> Os valores na ordem é ${valores.sort((a,b) => a-b )}`
+
+
+
+
+
+
+
+
+
+  }
+}
---
+++ Sobre Date/date.js
@@ -0,0 +1,9 @@
+/*new Date (ano, mes, dia, hora, minuto, segundo , milissegundo)
+
+let data = new Date(1981, 4, 15, 20, 15, 5, 0)
+
+console.log(data) */
+
+let data = new Date()
+
+console.log(data.getTime ()) 
