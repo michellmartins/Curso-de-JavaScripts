ed76445d7eb88003a02931877530b229a03b817c
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-07-02 20:10:13 -0300

    desafio

--- DesafioFinal/script.js
+++ DesafioFinal/script.js
@@ -1,102 +1,10 @@
-let num = document.querySelector('#txtn')
-let lista = document.querySelector('#instab')
-let res = document.querySelector('#res')
-let valores = []
 
-function insNumero(n) {
+let tab = [3, 7] 
+let multi = [1,2,3,4,5,6,7,8,9,10]
 
-  if (Number(n) >= 1 && Number(n) <= 100) {
-
-    return true
-
-  } else {
-
-    return false
-  }
-}
-
-function insLista(n, l) {
-
-  if (l.indexOf(Number(n)) != -1) {
-
-    return true
-
-  } else {
-
-    return false
-  }
-}
-
-
-function analisar() {
-
-  if (insNumero(num.value) && !insLista(num.value, valores)) {
-
-    valores.push(Number(num.value))
-    item = document.createElement('option')
-    item.text = `O valor é ${num.value}`
-    lista.append(item)
-
-
-  } else {
-
-    alert('ERRO - Valor indevido ou Valor já inserido')
-  }
-
-  num.value = " "
-  num.focus()
-}
-
-function finalizar() {
-
-  let total = valores.length
-  let maior = valores[0]
-  let menor = valores[0]
-  let soma = 0
-  let media = 0
-
-
-
-  if (total == 0) {
-
-    alert('Favor digitar enter 1 a 100')
-
-  } else {
-
+for (indexT = 0 ; indexT < tab.length ; indexT++) {
   
+  for ( indexM = 0 ; indexM < multi.length ; indexM++) 
 
-    for (let pos in valores) {
-
-      if (valores[pos] > maior) {
-
-        maior = valores[pos]
-
-      }
-
-      if (valores[pos] < menor) {
-
-        menor = valores[pos]
-      }
-
-      soma = soma + valores[pos]
-      media = soma / total
-    }
-
-    res.innerHTML = `Os Números no total foram ${total}`
-    res.innerHTML += `<p> O maior numero é ${maior}`
-    res.innerHTML += `<p> O menor numero é ${menor}`
-    res.innerHTML += `<p> O Soma dos valores é ${soma}`
-    res.innerHTML += `<p> A média dos valores é ${media}`
-    res.innerHTML += `<p> Os valores na sequencia é ${valores}`
-    res.innerHTML += `<p> Os valores na ordem é ${valores.sort((a,b) => a-b )}`
-
-
-
-
-
-
-
-
-
-  }
+    console.log(`${tab[indexT]} x ${multi[indexM]} = ${tab[indexT]*multi[indexM]}`)
 }
--- Sobre Date/date.js
+++ Sobre Date/date.js
@@ -4,6 +4,20 @@ let data = new Date(1981, 4, 15, 20, 15, 5, 0)
 
 console.log(data) */
 
-let data = new Date()
-
-console.log(data.getTime ()) 
+Date.prototype.getMesEmPortugues = function() {
+    if (this.getMonth() == 0){this.mesEmPortugues = "Janeiro"};
+    if (this.getMonth() == 1){this.mesEmPortugues = "Fevereiro"};
+    if (this.getMonth() == 2){this.mesEmPortugues = "Março"};
+    if (this.getMonth() == 3){this.mesEmPortugues = "Abril"};
+    if (this.getMonth() == 4){this.mesEmPortugues = "Maio"};
+    if (this.getMonth() == 5){this.mesEmPortugues = "Junho"};
+    if (this.getMonth() == 6){this.mesEmPortugues = "Julho"};
+    if (this.getMonth() == 7){this.mesEmPortugues = "Agosto"};
+    if (this.getMonth() == 8){this.mesEmPortugues = "Setembro"};
+    if (this.getMonth() == 9){this.mesEmPortugues = "Outubro"};
+    if (this.getMonth() == 10){this.mesEmPortugues = "Novembro"};
+    if (this.getMonth() == 11){this.mesEmPortugues = "Dezembro"};
+};
+var data = new Date();
+data.getMesEmPortugues();
+console.log("O mês atual é : " + data.mesEmPortugues);
