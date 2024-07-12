3c00b9bf4db4c9f706f1c5cdadd17c7b585abb1e
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-07-11 22:07:12 -0300

    desafio

--- DesafioFinal/script.js
+++ DesafioFinal/script.js
@@ -1,11 +1,94 @@
-let tabuada  =  [3, 7]
-let mult = [1,2,3,4,5,6,7,8,9,10]
+let num = document.querySelector('#txtn')
+let lista = document.querySelector('#instab')
+let res = document.querySelector('#res')
+let valores = []
 
+function insNumero(n) {
 
-for (indexT = 0 ; indexT < tabuada.length; indexT++) {
-  for (indexM = 0 ; indexM < mult.length ; indexM++) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
 
-  console.log(`${tabuada[indexT]} x ${mult[indexM]} = ${tabuada[indexT] * mult[indexM]}`)
+        return true
+    
+    } else {
+
+        return false
+    } 
 }
 
+function insLista (n,l) {
+
+    if (l.lastIndexOf(Number(n)) != -1) {
+
+        return true 
+    } else {
+
+        return false
+    } 
+
 }
+
+function analisar() {
+
+    if (insNumero(num.value) && !insLista(num.value,valores) ) {
+
+        valores.push(Number(num.value))
+        item = document.createElement('option')
+        item.text = `O valor é ${num.value}`
+        lista.appendChild(item)
+        res.innerHTML = " "
+    
+    } else {
+
+        alert ('ERRO - Valor ja inserido ou invalido')
+    } 
+
+    num.value = " "
+    num.focus()
+} 
+
+function finalizar () {
+
+    let total = valores.length 
+    let maior = valores[0]
+    let menor = valores[0]
+    let soma = 0
+    let media = 0
+
+ 
+    if ( total == 0 ) {
+
+        alert ( 'Favor digitar entre 1 e 100')
+
+    }else {
+
+    for ( pos in valores) {
+
+        if ( valores[pos] > maior) {
+
+            maior = valores[pos]
+        }
+        if (valores[pos] < menor) {
+
+            menor =  valores[pos]
+        }
+
+        soma = soma + valores[pos]
+        media = soma / total
+    }
+
+    }
+
+    res.innerHTML = " "
+    res.innerHTML = `O total de numero são ${total}`
+    res.innerHTML += `<p> O maior numero é ${maior}`
+    res.innerHTML += `<p> O menor numero é ${menor}`
+    res.innerHTML += `<p> A soma dos valores é ${soma}`
+    res.innerHTML += `<p> A média dos valores é ${media}`
+    res.innerHTML += `<p> Os valores digitados na ordem ${valores}`
+    res.innerHTML += `<p> Os valores na Ordem crescente ${valores.sort((a,b) => a - b )}`
+
+
+
+
+}
+  
---
+++ DesafioFinal/script2.js
@@ -0,0 +1,12 @@
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('Digite um numero', nome => {
+
+console.log(nome)
+readline.close();
+
+})dd
