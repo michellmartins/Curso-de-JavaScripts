f34f4287e2a61cc21464cf7b7dde6533200d2ae6
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-07-28 19:19:51 -0300

    desafio final

--- DesafioFinal/script.js
+++ DesafioFinal/script.js
@@ -3,94 +3,14 @@ let lista = document.querySelector('#instab')
 let res = document.querySelector('#res')
 let valores = []
 
-function insNumero (n) {
+function analisar () {
 
-    if (Number(n) >= 1 && Number(n) <= 100) {
+    if(insNumero(num.value) && !insLista(num.value,valores)) {
 
-        return true
-    } else {
-
-        return false
-    }
-}
-
-function insLista (n,l) {
-
-    if(l.lastIndexOf(Number(n)) != -1) {
-
-        return true
-    
-    }else {
-
-        return false
-    } 
-
-}
-
-function analisar() {
-
-    if (insNumero(num.value) && !insLista(num.value,valores)) {
-
-        valores.push(Number(num.value))
-        item = document.createElement('option')
-        item.text = `O valor é ${num.value}`
-        lista.appendChild(item)
-        res.innerHTML = " "
+        alert ('Valor ok')
 
     }else {
 
-        alert (' Valor indevido ou ja inserido')
+        alert (' Valor invalido ou ja adcionado')
     }
-
-    num.value = ' '
-    num.focus()
-}
-
-function finalizar() {
-
-    total = valores.length
-    maior = valores[0]
-    menor = valores[0]
-    soma = 0
-    media = 0 
-
-    if (total == 0) {
-
-        alert(' Favor digitar entre 1 e 100')
-
-    }else {
-
-        for (pos in valores) {
-
-            if (valores[pos] > maior) {
-
-                maior = valores[pos]
-            }
-
-            if (valores[pos] < menor) {
-
-                menor = valores[pos]
-            }
-
-            soma += valores[pos]
-            media = soma /total
-        }
-        
-       
-    }
-
-    res.innerHTML = " "
-    res.innerHTML = `Os valores digitados foram ${total}`
-    res.innerHTML += `<p> O maior valor é ${maior}`
-    res.innerHTML += `<p> O menor valor é ${menor}`
-    res.innerHTML += `<p> A soma dos valores é ${soma}`
-    res.innerHTML += `<p> A media dos valores é ${media}`
-    res.innerHTML += `<p> Os valores na ordem são ${valores.join(' - ')}`
-    res.innerHTML += `<p> Os valores na ordem crescente são ${valores.sort((a,b) => a-b), valores.join(' - ')}`
-
-
-
-
-
-
 }
--- Sobre Map/map.js
+++ Sobre Map/map.js
@@ -9,3 +9,22 @@ function primeiraEMaiuscula(elemento) {
     return elemento.charAt(0).toUpperCase() + elemento.slice(1);
 }*/
 
+var arrayNomes = [ 
+    
+    {nome: 'Maria' , sobrenome: 'Souza' },
+    {nome: 'João' , sobrenome: 'Gonçalves'},
+    {nome: `Pedro`, sobrenome: `Silva`}]
+
+var nomesCompleto = arrayNomes.map(concatenarNomes) 
+
+console.log(nomesCompleto)
+
+function concatenarNomes (elemento) {
+
+    let nome = {}
+    
+    nome['nomesCompleto'] = `${elemento.nome} ${elemento.sobrenome}`
+    
+    return nome
+}
+
