2cf6a86053d6009590d222d94228d899604427ef
Michell Martins <61015538+michellmartins@users.noreply.github.com>
2024-08-17 22:49:10 -0300

    mult

--- DesafioFinal/script.js
+++ DesafioFinal/script.js
@@ -5,97 +5,87 @@ let valores = []
 
 function insNumero(n) {
 
-    if(Number(n) >= 1 && Number(n) <= 100 ) {
+    if (Number(n) >= 1 && Number(n <=100)) {
 
         return true
-    
-    }else {
-
-        return false
-    
-    }
-}
-
-function insLista (n,l) {
-
-    if (l.indexOf(Number(n)) != -1 ) {
-
-        return true 
 
     }else {
 
+        return false 
+    }
+
+}
+
+function insLista(n,l) {
+
+    if (l.indexOf(Number(n)) != -1 ){
+
+        return true
+
+    }else {
+
         return false
     }
 }
+
+
 function analisar () {
 
-    if  (insNumero(num.value) && !insLista(num.value,valores)) {
+    if (insNumero(num.value) && !insLista(num.value,valores)) {
 
-        valores.push(Number(num.value))
+        valores.push(Number(num.value)) 
         item = document.createElement('option')
         item.text = `O valor é ${num.value}`
         lista.appendChild(item)
         res.innerHTML = " "
-        
-
     
     }else {
 
-        alert('Valor já inserido ou inválido')
+        alert(`O valor já inserido ou invalido`)
+
     }
 
-    num.value = ' '
+    num.value = " "
     num.focus()
 
 }
 
-function finalizar () {
+function finalizar ( ) {
 
-    let total = valores.length 
-    let maior = valores[0]
-    let menor = valores[0]
-    let soma = 0
-    let media = 0
+    total = valores.length 
+    soma = 0
+    media = 0
+    maior = valores[0]
+    menor = valores[0]
 
+    for ( pos in valores ) {
 
-    if (total == 0) {
+        if (valores[pos] > maior) {
 
-        alert ('Favor digitar entre 1 a 100')
-
-    }else {
-
-        for (pos in valores) {
-
-            if (valores[pos] > maior) {
-
-                maior = valores[pos]
-            }
-
-            if (valores[pos] < menor ) {
-
-                menor = valores[pos]
-            }
-
-            soma += valores[pos]
-            media = soma / total
-
-
+            maior = valores[pos]
 
         }
+
+        if (valores[pos] < menor) {
+
+            menor = valores[pos]
+
+        }
+        soma += valores[pos]
+        media = soma / total
     }
 
-    res.innerHTML = " "
-    res.innerHTML += `O valores digitados no toal foram ${total}`
-    res.innerHTML += `<p> O maior valor é ${maior}`
-    res.innerHTML += `<p> O menor valor é ${menor}`
-    res.innerHTML += `<p> A soma dos valores é ${soma}`
-    res.innerHTML += `<p> A média dos valores é ${media}`
-    res.innerHTML += `<p> Os valores digitados na ordem foram ${valores.sort().join(" - ")}`
-    res.innerHTML += `<p> Os valores digitados na ordem crescente são ${valores.sort((a,b) => a - b )}`
+    res.innerHTML += `Os valores no total digitados ${total}`
+    res.innerHTML += `<p> O maior é ${maior}`
+    res.innerHTML += `<p> O Menor é ${menor}`
+    res.innerHTML += `<p> A soma é ${soma}`
+    res.innerHTML += `<p> A média é ${media}`
+    res.innerHTML += `<p> Os numeros digitados na ordem foram ${valores.join(' - ')}`
+    res.innerHTML += `<p> Os numeros digitados na ordem crescente ${valores.sort((a,b) => a - b )}`
+    
 
 
 
 
 
 }
-
--- DesafioFinal/script2.js
+++ DesafioFinal/script2.js
@@ -1,20 +1,13 @@
-let megaSena = [ 1 , 23, 45, 11 , 74,  5]
-
-let jogados = [ 2 , 24 , 46 , 11, 73 , 5]
-
-let acertados = []
-
-for ( indexMega = 0 ; indexMega < megaSena.length ; indexMega++ ) {
-
-    for (indexJog = 0 ; indexJog < jogados.length ; indexJog++) {
-
-        if ( megaSena[indexMega] === jogados[indexJog]) {
-
-            acertados.push(megaSena[indexMega])
-        }
-    }
+let mult = [ 3 , 7]
+let vezes = [ 1,2,3,4,5,6,7,8,9,10]
 
 
+for (indexM = 0 ; indexM < mult.length ; indexM++) {
+     
+    for (indexV = 0 ; indexV < vezes.length ; indexV++) {
+
+    console.log(`${mult[indexM]} x ${vezes[indexV]} = ${mult[indexM] * vezes[indexV]} ` )
+    
 }
 
-console.log(acertados.join(" % "))
+}
